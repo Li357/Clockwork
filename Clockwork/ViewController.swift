e8778b161f4dbc40340aed5d3c6ff996841dd6b8
Andrew Li <andrewli357@gmail.com>
2022-01-03 23:28:10 -0500

    Initial commit

---
+++ Clockwork/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Clockwork
+//
+//  Created by Andrew Li on 1/3/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
